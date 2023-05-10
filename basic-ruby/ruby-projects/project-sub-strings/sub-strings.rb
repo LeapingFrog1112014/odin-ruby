0605d1e0b27d4094328a8653f956ee151c355609
Dilan C. Tern <127003284+dct-98@users.noreply.github.com>
2023-05-09 17:37:16 -0700

    Finished project

--- basic-ruby/ruby-projects/project-sub-strings/sub-strings.rb
+++ basic-ruby/ruby-projects/project-sub-strings/sub-strings.rb
@@ -3,3 +3,17 @@
 
 # String => Hash
 # Given a string will check every word in string that is also in dictionary and return a hash of word: occurences 
+
+def substrings (string, dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
+    
+    hashSubstrings = Hash.new(0)
+    downString = string.downcase
+
+    dictionary.each do |word|
+        matches = downString.scan(word).length
+        if matches != 0
+            hashSubstrings[word] = matches
+        end
+    end
+    return hashSubstrings
+end
