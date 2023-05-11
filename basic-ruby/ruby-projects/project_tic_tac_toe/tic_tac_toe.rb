00fa0337da82bc588eabacbbf4c28cb3beacd631
Dilan C. Tern <127003284+dct-98@users.noreply.github.com>
2023-05-10 21:21:16 -0700

    Created template for project

--- basic-ruby/ruby-projects/project_tic_tac_toe/tic_tac_toe.rb
+++ basic-ruby/ruby-projects/project_tic_tac_toe/tic_tac_toe.rb
@@ -1,16 +1,53 @@
 # tic_tac_toe
 
 module TicTacToe
-    GAMEBOARD = Array.new(3) {Array.new(3)}
 
-    def print_game
-        puts " #{GAMEBOARD[0][0]} | #{GAMEBOARD[0][1]} | #{GAMEBOARD[0][2]} "
-        puts "--+--+--"
-        puts " #{GAMEBOARD[1][0]} | #{GAMEBOARD[1][1]} | #{GAMEBOARD[1][2]} "
-        puts "--+--+--"
-        puts " #{GAMEBOARD[2][0]} | #{GAMEBOARD[2][1]} | #{GAMEBOARD[2][2]} "
-    end
+    BASE_BOARD = [ [1,2,3], [4,5,6], [7,8,9] ]
 
-end
+    class Game
+        
+        def initialize
+
+            @board = BASE_BOARD.to_a
+            @players = [Player.new(1, "X"), Player.new[0, "O"]]
+            current_player = @players.
+            print_board
+            
+        end
+
+        def print_board
+
+            puts " #{@board[0][1]} | #{@board[0][1]} | #{@board[0][2]} "
+            puts "--+--+--"
+            puts " #{@board[1][1]} | #{@board[1][1]} | #{@board[1][2]} "
+            puts "--+--+--"
+            puts " #{@board[2][1]} | #{@board[2][1]} | #{@board[2][2]} "
+
+        end
+
+        def check_won?
+        end
+
+
+        def place_piece
+        end
+        
+        def switch_turn
+        end
+
+        class Player
+
+            attr_accessor :current_player, :piece
     
-play = TicTacToe.new.PrintGame
+            def initialize(c, p)
+                @current_player = c
+                @piece = p
+            end
+    
+        end
+
+    end
+end
+
+
+    
